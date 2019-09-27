452412a16a0f7660820a8b1d47093c83fb4cbfca
David Hull <david.hull37@gmail.com>
2019-09-27 15:58:42 -0500

    Made cli.js executable by just typing 'create-pretty'

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const path = require('path');
+const program = require('commander');
+
+const VERSION = require('../package').version;
+
+console.log(fs);
+console.log(path);
+
+const main = () => {
+  console.log(`this is running`);
+  console.log(program.args);
+};
+
+program
+  .name('create-pretty')
+  .version(VERSION, ' --version')
+  .usage('[dir]')
+  .parse(process.argv);
+
+main();
