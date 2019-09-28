b1516003c1be54a9eedacf9b77c7318c4c676263
David Hull <david.hull37@gmail.com>
2019-09-28 16:34:32 -0500

    It works! But only for me right now

--- bin/cli.js
+++ bin/cli.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 
-// const fs = require('fs-extra');
-// const path = require('path');
+const fs = require('fs-extra');
+const path = require('path');
 const program = require('commander');
 
 const VERSION = require('../package').version;
-
-// console.log(fs);
-// console.log(path);
-
-const main = () => {
-  console.log(`this is running`);
-  console.log(program.args);
-};
+const templateDir = path.join(__dirname, '..', 'templates');
+const testDir = path.join(__dirname, '..', 'test');
 
 program
   .name('create-boiler')
   .version(VERSION, ' --version')
   .usage('[dir]')
   .parse(process.argv);
+
+// TODO: Create function that parses user directory
+// const userDir = () => {};
+const copy = () => {
+  return fs.copy(templateDir, testDir, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('FUCK YEAH IT WORKED');
+  });
+};
+
+const main = () => {
+  console.log(`this is running`);
+  console.log(templateDir);
+  console.log(testDir);
+  console.log(program.args);
+  copy();
+};
 
 main();
