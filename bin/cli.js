76779374e8cf287f00e010574daeb87523375789
David Hull <david.hull37@gmail.com>
2019-10-30 16:10:23 -0500

    Added create directory function

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 const VERSION = require('../package').version;
-const templateDir = path.join(__dirname, '..', 'templates');
+// const templateDir = path.join(__dirname, '..', 'templates');
 // const testDir = path.join(__dirname, '..', 'test');
 
 program
@@ -15,33 +15,42 @@ program
   .usage('[dir]')
   .parse(process.argv);
 
-// TODO: Finish this function
-const getUserDir = () => {
-  let inpDir = path.join(__dirname, '..', process.argv[0]);
-  if (process.argv.length === 0) {
-    console.log(chalk.red('Nothing was entered'));
-    inpDir = process.cwd();
+const createDirectory = (path) => {
+  if (fs.existsSync(path)) {
+    console.log('It does exists');
+    return 'true';
+  } else {
+    console.log(`Making directory at: ${path}`);
+    fs.mkdirSync(path);
   }
-  console.log(chalk.green(`Installing at: ${inpDir}`));
-  return inpDir;
 };
 
-const copy = (installDir, templateDir) => {
-  fs.copy(templateDir, installDir, (err) => {
-    if (err) {
-      return console.error(err);
-    }
-    console.log(`Andddddd it worked. Installed Directory: ${process.cwd()}`);
-  });
-  return console.log(`Andddddd it worked. Installed Directory: ${installDir}`);
+const userDir = () => {
+  console.log(process.argv.length);
+  let inpDir = path.join(__dirname, '..', `/default`);
+  if (process.argv.length > 2) {
+    inpDir = path.join(__dirname, '..', process.argv[2]);
+  }
+  console.log(chalk.green(`Installing at: ${inpDir}`));
+  createDirectory(inpDir);
 };
+
+// const copy = (installDir, templateDir) => {
+//   // fs.copy(templateDir, installDir, (err) => {
+//   //   if (err) {
+//   //     return console.error(err);
+//   //   }
+//   //   // console.log(`Andddddd it worked. Installed Directory: ${installDir}`);
+//   // });
+//   return console.log(`Andddddd it worked. Installed Directory: ${installDir}`);
+// };
 
 const main = () => {
   console.log(process.argv);
-  const userDirectory = getUserDir();
-  console.log(`aye its working!`);
   console.log('Current directory: ' + chalk.blue(process.cwd()));
-  copy(userDirectory, templateDir);
+  // copy(userDirectory, templateDir);
+  userDir();
+  console.log(chalk.red(process.argv[2]));
 };
 
 main();
